19d0079b441e9df3aa1d4d6672d00ddb91a59e47
hherin <hherin@student.42.fr>
2020-09-11 16:48:56 +0200

    wp phpmyadmin done

--- mysql/start-mysql.sh
+++ mysql/start-mysql.sh
@@ -1,13 +1,9 @@
 #! /bin/sh
 /usr/bin/mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql
+sleep 5
 mysqld --user=root &
-mariadb < /home/create_tables.sql
-mariadb -u root -e "CREATE USER '$PMA_USER'@'%' IDENTIFIED BY '$PMA_USER';"
-mariadb -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$PMA_USER'@'%' IDENTIFIED BY '$PMA_PWD' WITH GRANT OPTION;"
-# mariadb -u root -e "CREATE DATABASE wp1;"
-# mariadb -u root -e "USE wp1;"
-# mariadb -u root -e "GRANT ALL PRIVILEGES ON wp1.* to '$WRD_USER'@'localhost' IDENTIFIED BY '$WRD_PWD';"
-# mariadb -u root -e "FLUSH PRIVILEGES;"
+sleep 5
+mysql -u root < /home/create_tables.sql
 while true
 	do
 		sleep 1000;
--- setup.sh
+++ setup.sh
@@ -2,13 +2,17 @@
 minikube start
 sleep 5
 eval $(minikube docker-env)
+
 Kubectl apply -f metallb/metallb.yaml
-minikube ip
 kubectl apply -f metallb/configmap.yaml
+
 docker build -t nginx nginx/.
 docker build -t mysql mysql/.
 docker build -t pma phpmyadmin/.
+docker build -t wp wordpress/.
+
 kubectl apply -f nginx/nginx-pod.yaml
 kubectl apply -f phpmyadmin/phpmyadmin-pod.yaml
 kubectl apply -f mysql/mysql-pod.yaml
+kubectl apply -f wordpress/wp-pod.yaml
 #minikube dashboard
