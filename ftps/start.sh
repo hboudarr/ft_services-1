ca3fac4465afa7c10ecb02b43175e0fb75f36e0c
hherin <hherin@student.42.fr>
2020-09-16 12:35:39 +0200

    ftp done

---
+++ ftps/start.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+openssl req -x509 -nodes -days 365 -subj "/C=CA/ST=QC/O=Company, Inc./CN=mydomain.com" -newkey rsa:2048 -keyout /etc/vsftpd/ssl/vsftpd.key -out /etc/vsftpd/ssl/vsftpd.crt
+sleep 5
+
+chown "nobody:nogroup" /home/hherin/ftp
+chown "hherin:hherin" /home/hherin/ftp/files
+
+# test file for download
+echo "this is a test file" > /home/hherin/ftp/files/test.txt
+
+/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf &
+
+while true
+	do
+		sleep 1000;
+	done
--- setup.sh
+++ setup.sh
@@ -10,9 +10,11 @@ docker build -t nginx nginx/.
 docker build -t mysql mysql/.
 docker build -t pma phpmyadmin/.
 docker build -t wp wordpress/.
+docker build -t ftps ftps/.
 
 kubectl apply -f nginx/nginx-pod.yaml
 kubectl apply -f phpmyadmin/phpmyadmin-pod.yaml
 kubectl apply -f mysql/mysql-pod.yaml
 kubectl apply -f wordpress/wp-pod.yaml
+kubectl apply -f ftps/ftps-pod.yaml
 #minikube dashboard
